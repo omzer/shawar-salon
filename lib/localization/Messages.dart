edb203ad7df9fd35c18182e51c7947cd4dbcd95b
Omar Sabri <oz9.sa7@gmail.com>
2020-12-08 16:25:47 +0200

    Add english local support

--- lib/localization/Messages.dart
+++ lib/localization/Messages.dart
@@ -22,6 +22,26 @@ class Messages extends Translations {
           'stay_on_page': 'البقاء في الصفحة',
           'confirm_back_msg':
               'يوجد لديك تعديلات لم تقم بحفظها, هل أنت متأكد من أنك تريد الرجوع؟',
-        }
+        },
+        'en': {
+          'salon_name': 'Shawar\'s Salon',
+          'book': 'Book',
+          'cancel': 'Cancel',
+          'shave_count_message': 'You shaved %s times before',
+          'connection_error': 'Connection error, please try again later',
+          'loading_success': 'Loaded successfully',
+          'profile': 'Profile info',
+          'working_hours': 'Working hours',
+          'archive': 'History',
+          'settings': 'App settings',
+          'customer_name': 'Customer name',
+          'phone_number': 'Phone number',
+          'be_aware': 'Be aware',
+          'enter_valid_data': 'Please enter valid user name and phone number',
+          'back': 'back',
+          'stay_on_page': 'Stay on the page',
+          'confirm_back_msg':
+              'You have unsaved changes, are you sure you want to go back without saving them?',
+        },
       };
 }
